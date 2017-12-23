4500b5dd8d1ac83110178cba093f4a1846fc509e
AdGw <adrian.gwozdziej@gmail.com>
2017-12-23 17:26:46 +0100

    Refactoring React calculator

---
+++ Reactt/Calculator/src/Components/Button/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export const Button= ({text, buttonClass, clickHandler}) => (
+  <button className={buttonClass}
+  onClick={() => clickHandler ? clickHandler.call(null, text) : null}>
+  <span className="title">{text}</span>
+  </button>
+);
+
+export default Button;
