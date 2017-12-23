d09d56d74057a422f5279842f1217e389e81f333
AdGw <adrian.gwozdziej@gmail.com>
2017-12-23 17:29:01 +0100

    Deleted some files

--- Reactt/Calculator/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Calculator from './Components/Calculator';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Calculator />, document.getElementById('root'));
-registerServiceWorker();
