914c3837e9f6c41f3a25429cae017b2602226036
Adgw <adrian.gwozdziej@gmail.com>
2018-03-15 15:51:48 +0100

    Add basic structure of connecting angular with firebase app

---
+++ Angular/Listening/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ListingsComponent } from './Components/listings/listings.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ListingComponent } from './Components/listing/listing.component';
+import { AddListingComponent } from './Components/add-listing/add-listing.component';
+import { EditListingComponent } from './Components/edit-listing/edit-listing.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listings', component: ListingsComponent},
+  {path: 'add-listing', component: AddListingComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
