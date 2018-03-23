fd8c7d9b7f81be44c74b36c0b1b58e92fd67308f
Adgw <adrian.gwozdziej@gmail.com>
2018-03-23 20:07:13 +0100

    Display information about type of product, details

--- Angular/Listening/src/app/Components/navbar/navbar.component.ts
+++ Angular/Listening/src/app/Components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
-//import { FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +10,6 @@ export class NavbarComponent implements OnInit {
 
   constructor(
   public af:AngularFire
-  //public flashMessage:FlashMessagesService
   	) { }
 
   ngOnInit() {
@@ -22,9 +20,6 @@ export class NavbarComponent implements OnInit {
   }
   logout(){
   	this.af.auth.logout();
-  	// this.flashMessage.show('You are logged out',
-  	// {cssClass: 'alert-success', timeout: 1000}
-  	//);
   }
 
 }
--- Angular/Listening/src/app/services/firebase.service.ts
+++ Angular/Listening/src/app/services/firebase.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, AngularFireDatabase, FirebaseListObservable } from 'angularfire2';﻿
+import { AngularFire, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';﻿
 
 @Injectable()
 export class FirebaseService {
+  listing: FirebaseObjectObservable<any[]>;
   listings: FirebaseListObservable<any[]>;
   constructor(private af: AngularFireDatabase) { }
+
   getListings(){
   	this.listings = this.af.list('/listings') as FirebaseListObservable<Listing>
   	return this.listings;
   }
+
+  getListingDetails(id){
+  	this.listing = this.af.object('/listings/'+id) as FirebaseObjectObservable<Listing>
+  	return this.listing;
+  }
+
 }
 
 interface Listing{
