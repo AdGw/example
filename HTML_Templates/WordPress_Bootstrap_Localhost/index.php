09fdcf13a257cfc79a1794e09034fdff346c85a1
AdGw <adrian.gwozdziej@gmail.com>
2018-04-08 14:24:55 +0200

    Create Wordpress template via localhost, index splitted into parts

---
+++ HTML_Templates/WordPress_Bootstrap_Localhost/index.php
@@ -0,0 +1,33 @@
+<?php get_header();?>
+    <section class ="showcase">
+      <div class="container">
+        <h1>Custom Bootstrap Template</h1>
+        <p>Vivamus tempor vel neque eu porta. Etiam aliquet, quam nec ullamcorper mattis, est nibh ornare odio, eget pretium sapien massa ut elit. Suspendisse turpis odio, varius vitae tortor sed, commodo mollis massa. Sed in faucibus arcu. Maecenas congue blandit nulla, eu ullamcorper ligula auctor a. Suspendisse quam lacus, consectetur at auctor eu, lacinia eget diam. Vestibulum bibendum neque ac velit viverra tincidunt. Ut suscipit bibendum lectus. In sed facilisis dui. Maecenas id tortor non ligula faucibus finibus et vitae tellus. Praesent tortor nibh, laoreet sed odio sed, consequat aliquet augue. Morbi vel imperdiet ipsum. Nulla facilisi. Mauris vel odio elit. Phasellus at fringilla neque, et dapibus risus.</p>
+        <a class="btn btn-primary btn-lg">Read More</a> 
+      </div>
+    </section>
+    <section class="boxes">
+      <div class="container">
+        <div class="row">
+          <div class ="col-md-4">
+            <div class="box">
+              <i class="fa fa-address-book" aria-hidden ="true"></i>
+              <h3><p>ligula faucibus finibus et vitae tellus. Praesent tortor nibh, laoreet sed odio sed, consequat aliquet augue. Morbi vel imperdiet</p></h3>
+            </div>   
+          </div>
+          <div class ="col-md-4">
+            <div class="box">
+              <i class="fa fa-gears" aria-hidden ="true"></i>
+              <h3><p>ligula faucibus finibus et vitae tellus. Praesent tortor nibh, laoreet sed odio sed, consequat aliquet augue. Morbi vel imperdiet</p></h3>
+            </div>   
+          </div>
+          <div class ="col-md-4">
+            <div class="box">
+              <i class="fa fa-search" aria-hidden ="true"></i>
+              <h3><p>ligula faucibus finibus et vitae tellus. Praesent tortor nibh, laoreet sed odio sed, consequat aliquet augue. Morbi vel imperdiet</p></h3>
+            </div>   
+          </div>
+        </div>
+      </div>
+    </section>
+<?php get_footer();?>
