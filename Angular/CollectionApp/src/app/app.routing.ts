ca31cc2ec090d641265047af4797392b034b2ff7
Adgw <adrian.gwozdziej@gmail.com>
2018-04-09 14:27:28 +0200

    Add routing feature

---
+++ Angular/CollectionApp/src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from "@angular/router";
+import { CollectionComponent } from "./collection/collection.component"
+import { MarketComponent } from "./market/market.component"
+
+const APP_ROUTES: Routes =[
+{ path: '', redirectTo: '/collection', pathMatch: 'full' },
+{ path: 'collection', component: CollectionComponent },
+{ path: 'market', component: MarketComponent }
+];
+
+RouterModule.forRoot(APP_ROUTES);
+
+export const routing = RouterModule.forRoot(APP_ROUTES) 
