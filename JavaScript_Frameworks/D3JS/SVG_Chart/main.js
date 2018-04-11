297290818ed5385bd76988121f348c63b332ee6d
Adgw <adrian.gwozdziej@gmail.com>
2018-04-11 12:37:52 +0200

    Add hover feature to get a data information from chart

--- JavaScript_Frameworks/D3JS/SVG_Chart/main.js
+++ JavaScript_Frameworks/D3JS/SVG_Chart/main.js
@@ -1,15 +1,21 @@
 let data = [10,20,40,500,360,20,8,260,1,20,30,48,600,];
-
-let height = 500;
-let width = 500;
+	height = 500;
+	width = 500;
+	tool = d3.select('body').append('div')
+		.style('position','absolute')
+		.style('background','#ccc')
+		.style('padding','3 10px')
+		.style('border','2px #333 solid')
+		.style('border-radius','5px')
+		.style('opacity', '0')
 
 let yScale = d3.scaleLinear();
-yScale.domain([0, d3.max(data)]);
-yScale.range([0, height]);
+	yScale.domain([0, d3.max(data)]);
+	yScale.range([0, height]);
 
 let xScale = d3.scaleBand();
-xScale.domain(d3.range(0, data.length));
-xScale.range([0, width]);
+	xScale.domain(d3.range(0, data.length));
+	xScale.range([0, width]);
 
 let colorScale = d3.scaleLinear();
 	colorScale.domain([0, data.length]);
@@ -28,3 +34,17 @@ let myChart = d3.select('#chart').append('svg')
         .attr('x',((d, i) => xScale(i)))
         .attr('height', ((d)=> yScale(d)))
         .attr('y',((d)=> height - yScale(d)))
+    .on('mouseover',function(d){
+    	tool.transition()
+    		.style('opacity',1)
+    	tool.html(d)
+    		.style('left',(d3.event.pageX + 'px'))
+    		.style('top', (d3.event.pageY + 'px'))
+    	d3.select(this).style('opacity', 0.5)
+    })
+    .on('mouseout', function(d){
+    	tool.transition()
+    		.style('opacity',0)
+    	d3.select(this).style('opacity', 1)
+    })
+    
