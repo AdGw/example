4d56faac960ed707de216157909a3fb6c6a6f247
Adgw <adrian.gwozdziej@gmail.com>
2018-04-12 16:43:16 +0200

    Configurate node server add dust template

---
+++ NodeJS/RecipeBook/main.js
@@ -0,0 +1,24 @@
+let express = require('express'),
+	path = require('path'),
+	bodyParser = require('body-parser'),
+	cons = require('consolidate'),
+	dust = require('dustjs-helpers'),
+	pg = require('pg'),
+	app = express();
+	conString = "postgres://adgw:1234@localhost/recipedb";
+
+app.engine('dust', cons.dust);
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+app.listen(3000,function(){
+	console.log('x');
+});
