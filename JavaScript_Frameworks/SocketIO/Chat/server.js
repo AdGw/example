d6be6a03236b725e8747efd42fbce64d51fc4538
Adgw <adrian.gwozdziej@gmail.com>
2018-04-26 15:48:08 +0200

    Add fully connection beetwen two users in real time

--- JavaScript_Frameworks/SocketIO/Chat/server.js
+++ JavaScript_Frameworks/SocketIO/Chat/server.js
@@ -18,13 +18,25 @@ io.sockets.on('connection', function(socket){
   console.log("Connected: %s", connections.length);
 
   //Disconnect
-  socket.on('Disconnect', function(data){
-    connections.splice(connections.indexOf(socket),1);
-    console.log('Diconnected: %s', connections.length)
-  })
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+      users.splice(users.indexOf(socket.username), 1)
+      updateUsernames();
+      connections.splice(connections.indexOf(socket),1);
+      console.log('Diconnected: %s', connections.length)
+  });
   //Send Message
   socket.on('send message', function(data){
-    console.log(data);
-    io.sockets.emit('new message', {msg: data})
-  })
+    io.sockets.emit('new message', {msg: data, user: socket.username})
+  });
+  //New user
+  socket.on('new user', function(data, cb){
+    cb(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+  function updateUsernames(){
+    io.sockets.emit('get users', users);
+  }
 })
