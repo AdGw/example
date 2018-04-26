6c2982629b0dda1a6abce7ee4b382955211d6c97
Adgw <adrian.gwozdziej@gmail.com>
2018-04-26 15:22:31 +0200

    Add connection between users, configurate socketIO

--- JavaScript_Frameworks/SocketIO/Chat/server.js
+++ JavaScript_Frameworks/SocketIO/Chat/server.js
@@ -3,11 +3,28 @@ let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
 
-users=[];
-connections =[];
+let users=[];
+let connections =[];
 
 server.listen(process.env.PORT || 3000);
-console.log('x')
+console.log('Server running');
+
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log("Connected: %s", connections.length);
+
+  //Disconnect
+  socket.on('Disconnect', function(data){
+    connections.splice(connections.indexOf(socket),1);
+    console.log('Diconnected: %s', connections.length)
+  })
+  //Send Message
+  socket.on('send message', function(data){
+    console.log(data);
+    io.sockets.emit('new message', {msg: data})
+  })
+})
