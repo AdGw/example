8f7d6ad97c9c20ea8efaba1ea515c304f703cf52
AdGw <adrian.gwozdziej@gmail.com>
2018-04-30 21:26:42 +0200

    Change char for every frame, randomly length

--- JavaScript_Examples/Matrix_Effect/sketch.js
+++ JavaScript_Examples/Matrix_Effect/sketch.js
@@ -1,5 +1,5 @@
-let symbol;
-	symbolSize = 20;
+let	symbolSize = 20;
+	stream;
 
 function setup(){
 	createCanvas(
@@ -7,17 +7,14 @@ function setup(){
 		window.innerHeight
 		);
 	background(0);
-	symbol = new Symbol(
-		width/2,
-		0,
-		random(5,10)
-	);
-	symbol.setToRandomSymbol();
-	textSize(symbolSize)
+	stream = new Stream();
+	stream.generateSymbols();
+	textSize(symbolSize);
 }
 
 function draw(){
-symbol.render();
+	background(0)
+	stream.render();
 }
 
 function Symbol(x,y,speed){
@@ -25,21 +22,42 @@ function Symbol(x,y,speed){
 	this.y = y;
 	this.value;
 	this.speed = speed;
+	this.switchInterval = round(random(2,20));
+
 	this.setToRandomSymbol=()=>{
+	if(frameCount % this.switchInterval == 0){
 		this.value = String.fromCharCode(
 			0x30A0 + round(random(0,96))
-		);
-	}
-	this.render =()=>{
-		fill(0,255,70)
-		text(this.value, this.x, this.y);
-		this.charSpeed();
+			);
+		}
 	}
 	this.charSpeed=()=>{
-		this.y += this.speed;
+		this.y = (this.y >=height) ? 0: this.y+=this.speed;
 	}
 }
 
 function Stream(){
+	this.symbols = [];
+	this.totalSymbols = round(random(5,30));
+	this.speed = random(5,20)
 
+	this.generateSymbols=()=>{
+		let y = 0;
+			x = width/2;
+
+		for(let i = 0 ; i <= this.totalSymbols ; i++){
+			symbol = new Symbol(x,y,this.speed);
+			symbol.setToRandomSymbol();
+			this.symbols.push(symbol);
+			y-= symbolSize;
+		}
+	}
+	this.render = ()=>{
+		this.symbols.forEach(function(symbol){
+			fill(0,255,70)
+			text(symbol.value, symbol.x, symbol.y);
+			symbol.charSpeed();
+			symbol.setToRandomSymbol();
+		});
+	}
 }
