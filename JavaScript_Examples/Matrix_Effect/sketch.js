1e840dfbd291dbe92527dd4519adf3d41d257f75
AdGw <adrian.gwozdziej@gmail.com>
2018-04-30 20:43:58 +0200

    Generate random char and display with displacement

---
+++ JavaScript_Examples/Matrix_Effect/sketch.js
@@ -0,0 +1,45 @@
+let symbol;
+	symbolSize = 20;
+
+function setup(){
+	createCanvas(
+		window.innerWidth,
+		window.innerHeight
+		);
+	background(0);
+	symbol = new Symbol(
+		width/2,
+		0,
+		random(5,10)
+	);
+	symbol.setToRandomSymbol();
+	textSize(symbolSize)
+}
+
+function draw(){
+symbol.render();
+}
+
+function Symbol(x,y,speed){
+	this.x = x;
+	this.y = y;
+	this.value;
+	this.speed = speed;
+	this.setToRandomSymbol=()=>{
+		this.value = String.fromCharCode(
+			0x30A0 + round(random(0,96))
+		);
+	}
+	this.render =()=>{
+		fill(0,255,70)
+		text(this.value, this.x, this.y);
+		this.charSpeed();
+	}
+	this.charSpeed=()=>{
+		this.y += this.speed;
+	}
+}
+
+function Stream(){
+
+}
