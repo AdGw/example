6a6c23e1101a677229ccc216fa9f4ae23fd6b39c
Adgw <adrian.gwozdziej@gmail.com>
2018-05-10 16:45:22 +0200

    split code into parts, add gamewon feature

---
+++ JavaScript_Frameworks/P5JS/Games/2048/grid.js
@@ -0,0 +1,63 @@
+const blankGrid=()=>[
+		[0,0,0,0],
+		[0,0,0,0],
+		[0,0,0,0],
+		[0,0,0,0]
+	];
+
+const compare=(a,b)=>{
+	for(let i = 0;i<4;i++){
+		for(let j = 0;j<4;j++){
+			if(a[i][j] !== b[i][j]){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+const copyGrid=(grid)=>{
+	let ex=blankGrid();
+		for(let i = 0;i<4;i++){
+			for(let j = 0;j<4;j++){
+				ex[i][j] = grid[i][j];
+			}
+		}
+		return ex;
+}
+
+const flipGrid=grid=>{
+	for(let i = 0; i<4; i++){
+		grid[i].reverse();
+	}
+	return grid;
+}
+
+const rotateGrid=grid=>{
+	let newGrid = blankGrid()
+	for(let i = 0; i<4; i++){
+		for(let j = 0; j<4; j++){
+			newGrid[i][j] = grid[j][i];
+		}
+	}
+	return newGrid;
+}
+
+const addNumber=()=>{
+	let options =[];
+	for(let i = 0;i<4;i++){
+		for(let j = 0;j<4;j++){
+			if(grid[i][j] === 0){
+				options.push({
+					x:i,
+					y:j
+				});
+			}
+		}
+	}
+	if(options.length > 0){
+  	let spot = random(options),
+  			r = random(1)
+  	grid[spot.x][spot.y] = r > 0.5 ? 2 : 4;
+  }
+}
--- JavaScript_Frameworks/P5JS/Games/2048/sketch.js
+++ JavaScript_Frameworks/P5JS/Games/2048/sketch.js
@@ -1,32 +1,6 @@
 let grid;
 let score = 0;
 
-const isGameOver=()=>{
-	for(let i = 0;i<4;i++){
-		for(let j = 0;j<4;j++){
-			if(grid[i][j] == 0){
-				return false;
-			}
-			if(i!== 3 && grid[i][j] === grid[i+1][j]){
-				return false;
-			}
-			if(j!== 3 && grid[i][j] === grid[i][j+1]){
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-const blankGrid=()=>{
-return[
-		[0,0,0,0],
-		[0,0,0,0],
-		[0,0,0,0],
-		[0,0,0,0]
-	];
-}
-
 function setup(){
 	createCanvas(800,800);
 	noLoop();
@@ -34,63 +8,6 @@ function setup(){
 	addNumber();
 	addNumber();
 	updateCanvas();
-}
-
-const addNumber=()=>{
-	let options =[];
-	for(let i = 0;i<4;i++){
-		for(let j = 0;j<4;j++){
-			if(grid[i][j] === 0){
-				options.push({
-					x:i,
-					y:j
-				});
-			}
-		}
-	}
-	if(options.length > 0){
-	let spot = random(options),
-			r = random(1)
-	grid[spot.x][spot.y] = r > 0.5 ? 2 : 4;
-	}
-}
-
-const compare=(a,b)=>{
-	for(let i = 0;i<4;i++){
-		for(let j = 0;j<4;j++){
-			if(a[i][j] !== b[i][j]){
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-const copyGrid=(grid)=>{
-	let ex=blankGrid();
-		for(let i = 0;i<4;i++){
-			for(let j = 0;j<4;j++){
-				ex[i][j] = grid[i][j];
-			}
-		}
-		return ex;
-}
-
-const flipGrid=grid=>{
-	for(let i = 0; i<4; i++){
-		grid[i].reverse();
-	}
-	return grid;
-}
-
-const rotateGrid=grid=>{
-	let newGrid = blankGrid()
-	for(let i = 0; i<4; i++){
-		for(let j = 0; j<4; j++){
-			newGrid[i][j] = grid[j][i];
-		}
-	}
-	return newGrid;
 }
 
 function keyPressed(){
@@ -134,41 +51,17 @@ function keyPressed(){
 		if(gameover){
 			alert("Game Over");
 		}
+		let gamewon = isGameWon();
+		if(gamewon){
+			alert("Game Over");
+		}
 	}
-}
-
-function operate(row){
-	row=slide(row);
-	row=combine(row);
-	row=slide(row);
-	return row;
 }
 
 function updateCanvas(){
 	background(255);
 	drawGrid();
 	select('#score').html(score);
-}
-
-const slide=row=>{
-	let arr = row.filter(value=>value),
-			missing = 4-arr.length,
-			zeros = Array(missing).fill(0);
-			arr = zeros.concat(arr);
-			return arr;
-}
-
-const combine=row=>{
-	for (let i = 3; i >=1; i--) {
-		let a = row[i],
-		 		b = row[i-1];
-		if(a===b){
-			row[i] = a+b;
-			score +=row[i];
-			row[i-1] = 0;
-		}
-	}
-	return row;
 }
 
 const drawGrid = () =>{
