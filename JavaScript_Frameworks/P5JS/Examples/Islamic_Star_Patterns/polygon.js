e6e8a01840684a368f5261bd2fb38b5674aacd0d
AdGw <adrian.gwozdziej@gmail.com>
2018-05-20 14:25:41 +0200

    create polygon from scratch

---
+++ JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/polygon.js
@@ -0,0 +1,29 @@
+function Polygon(){
+	this.vertices = [];
+	this.edges = [];
+
+	this.addVertex = (x,y)=>{
+		let a = createVector(x,y);
+		let	total = this.vertices.length;
+		if(total > 0){
+			let previous = this.vertices[total-1];
+			let edge = new Edge(previous,a);
+			this.edges.push(edge);
+		}
+		this.vertices.push(a);
+	}
+
+	this.close = ()=>{
+		let	total = this.vertices.length;
+		let last = this.vertices[total-1];
+		let first = this.vertices[0];
+		let edge = new Edge(last,first);
+		this.edges.push(edge);
+	}
+
+	this.show = ()=>{
+		for(let i in this.edges){
+			this.edges[i].show();
+		}
+	}
+}
---
+++ JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/sketch.js
@@ -0,0 +1,14 @@
+let poly;
+
+function setup(){
+	createCanvas(800,600);
+	background(50);
+	poly = new Polygon();
+	poly.addVertex(100,100);
+	poly.addVertex(200,100);
+	poly.addVertex(200,200);
+	poly.addVertex(100,200);
+	poly.close();
+	poly.show();
+}
+
