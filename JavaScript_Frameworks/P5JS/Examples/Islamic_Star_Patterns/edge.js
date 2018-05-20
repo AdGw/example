b13c422534b28a33022644e9d0cfb71947cc8b53
AdGw <adrian.gwozdziej@gmail.com>
2018-05-20 17:34:28 +0200

    Create pattern to whole canvas screen, add sliders

--- JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/edge.js
+++ JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/edge.js
@@ -15,11 +15,23 @@ function Edge(a, b){
 		mid.mult(0.5);
 		let v1 = p5.Vector.sub(this.a, mid);
 		let v2 = p5.Vector.sub(this.b, mid);
+		let offset1 = mid;
+		let offset2 = mid;
+		if(delta > 0){
+			v1.setMag(delta);
+			v2.setMag(delta);
+
+			offset1 = p5.Vector.add(mid,v2);
+			offset2 = p5.Vector.add(mid,v1);
+		}
+		v1.normalize();
+		v2.normalize();
+
 		v1.rotate(radians(-angle));
 		v2.rotate(radians(angle));
 
-		this.h1 = new Middle(mid,v1);
-		this.h2 = new Middle(mid,v2);
+		this.h1 = new Middle(offset1,v1);
+		this.h2 = new Middle(offset2,v2);
 	}
 
 	this.findEnds = (edge)=>{
--- JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/sketch.js
+++ JavaScript_Frameworks/P5JS/Examples/Islamic_Star_Patterns/sketch.js
@@ -1,17 +1,35 @@
-let poly,
-	angle = 60;
+let polys = [],
+	angle = 70,
+	delta = 10,
+	deltaSlider,
+	angleSlider;
 
 function setup(){
 	createCanvas(800,600);
+	
+	deltaSlider = createSlider(0,25,0);
+	angleSlider = createSlider(0,90,60);
+	let inc = 100;
+	for(let x = 0 ; x< width; x+= inc){
+		for(let y = 0 ; y< height; y+= inc){
+			let poly = new Polygon();
+			poly.addVertex(x,y);
+			poly.addVertex(x+inc,y);
+			poly.addVertex(x+inc,y+inc);
+			poly.addVertex(x,y+inc);
+			poly.close();
+			polys.push(poly)
+		}
+	}
+}
+
+function draw(){
 	background(50);
-	// angleMode(DEGREES);
-	poly = new Polygon();
-	poly.addVertex(100,100);
-	poly.addVertex(300,100);
-	poly.addVertex(300,300);
-	poly.addVertex(100,300);
-	poly.close();
-	poly.middle();
-	poly.show();
+	angle = angleSlider.value();
+	delta = deltaSlider.value();
+	for(let i in polys){
+		polys[i].middle();
+		polys[i].show();
+	}
 }
 
