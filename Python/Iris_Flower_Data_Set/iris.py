76d6e30459e590579e375da3b4c671bd1f70a2c2
AdGw <adrian.gwozdziej@gmail.com>
2018-05-28 11:45:01 +0200

    Add some prediction forumals to neighbours algorithm

--- Python/Iris_Flower_Data_Set/iris.py
+++ Python/Iris_Flower_Data_Set/iris.py
@@ -14,3 +14,23 @@ print(type(iris.data))
 print(type(iris.target))
 X = iris.data
 y = iris.target
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+print(knn)
+knn.fit(X,y)
+prediction = knn.predict([[2,4,3,1]])
+type(prediction)
+# [0] - setosa
+print(prediction)
+
+prediction2 = knn.predict([[2,4,3,1],[4,6,5,3]])
+# [0 2] - setosa, virginica
+print(prediction2)
+
+knn5 = KNeighborsClassifier(n_neighbors=5)
+knn5.fit(X,y)
+prediction = knn5.predict([[2,4,3,1],[4,6,5,3]])
+type(prediction)
+# [0 1] - setosa, versicolor
+print(prediction)
