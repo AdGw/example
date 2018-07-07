24aedc8ef79ebf59f22bc2aa0e7936bc2c18559d
Adgw <adrian.gwozdziej@gmail.com>
2018-07-06 20:47:34 -0400

    Configurate firebase database

--- JavaScript_Frameworks/P5JS/Examples/Color_Classifier/sketch.js
+++ JavaScript_Frameworks/P5JS/Examples/Color_Classifier/sketch.js
@@ -1,16 +1,36 @@
+let r,g,b;
+let database;
+function pickColor(){
+	r = floor(random(256));
+	g = floor(random(256));
+	b = floor(random(256));
+	background(r,g,b);
+}
 
 
 function setup(){
-	createCanvas(400,400)
-	let r = floor(random(256));
-	let g = floor(random(256));
-	let b = floor(random(256));
+	let config = {
+		apiKey: "AIzaSyCcq-9sW0-3WQ6zoLmzZ5UuXQLLebTRMbI",
+		authDomain: "color-classification-ab7d3.firebaseapp.com",
+		databaseURL: "https://color-classification-ab7d3.firebaseio.com",
+		projectId: "color-classification-ab7d3",
+		storageBucket: "color-classification-ab7d3.appspot.com",
+		messagingSenderId: "574671660934"
+	  };
+	firebase.initializeApp(config);
+	database = firebase.database();
 
-	background(r,g,b);
-	let dropdown = createSelect();
-	dropdown.option('red')
-	dropdown.option('green')
-	dropdown.option('blue')
+	createCanvas(400,400);
+	pickColor();
+
+	let buttons = [];
+	buttons.push(createButton('red'))
+	buttons.push(createButton('green'))
+	buttons.push(createButton('blue'))
+
+	for(let i in buttons){
+		buttons[i].mousePressed(sendData);
+	}
 
 	let submit = createButton("submit");
 	submit.mousePressed(sendData)
@@ -18,6 +38,27 @@ function setup(){
 
 }
 
-let sendData = () =>{
-		
+function sendData(){
+	console.log(this.html());
+	let colorDatabase = database.ref("colors");
+	let data = {
+		r: r,
+		g: g,
+		b: b,
+		label: this.html()
+	  };
+	console.log("saving");
+	console.log(data);
+	let color = colorDatabase.push(data, finished);
+	console.log("Firebase generate key: " + color.key)
+
+	function finished(err) {
+		if (err) {
+		  console.error("ooops, something went wrong.");
+		  console.error(err);
+		} else {
+		  console.log('Data saved successfully');
+		  pickColor();
+		}
+	}
 }
