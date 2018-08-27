4523d85d03a89dbe1faccbf55587f1c00c68ba2d
Adgw <adrian.gwozdziej@gmail.com>
2018-08-27 14:39:46 -0400

    Add example how to get data from database

---
+++ JavaScript_Examples/Get_Data_From_Database/load-comments.php
@@ -0,0 +1,16 @@
+<?php
+	include 'dbh.php';
+	$commentNewCount = $_POST['commentNewCount'];
+	$sql = "SELECT * FROM comments LIMIT $commentNewCount";
+	$result = mysqli_query($connection, $sql);
+	if(mysqli_num_rows($result)>0){
+		while($row = mysqli_fetch_assoc($result)){
+			echo "<p>";
+			echo $row['author'];
+			echo $row['message'];
+			echo "</p>";
+			}
+		}else{
+			echo "No comments";
+	}
+?>
