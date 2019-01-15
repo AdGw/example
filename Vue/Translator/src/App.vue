d64fb0916d0df19b57b1a45f29f8e9afd9160f49
Adrian Gwozdziej <adrian.gwozdziej@meelogic.com>
2019-01-15 13:53:09 +0100

    display translated text

--- Vue/Translator/src/App.vue
+++ Vue/Translator/src/App.vue
@@ -3,18 +3,36 @@
     <h1>Translator</h1>
     <h5>Powered by Vue.js</h5>
     <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translatedText"></TranslateOutput>
   </div>
 </template>
 
 <script>
-import TranlateForm from "./components/TranslateForm";
+import TranslateForm from "./components/TranslateForm";
+import TranslateOutput from "./components/TranslateOutput";
 export default {
   name: "App",
   components: {
-    TranlateForm
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function() {
+    return {
+      translatedText: ""
+    };
   },
   methods: {
-    translateText: function(text) {}
+    translateText: function(text) {
+      this.$http
+        .get(
+          "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190115T114934Z.eacaed35194efee2.f853a351e04382ea66dd8d76823814165d590438&lang=ru&text=" +
+            text
+        )
+        .then(response => {
+          console.log(response.body.text);
+          this.translatedText = response.body.text[0];
+        });
+    }
   }
 };
 </script>
