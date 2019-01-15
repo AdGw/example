410116753cb82067cd7ac2995b9ca79f3511565f
Adrian Gwozdziej <adrian.gwozdziej@meelogic.com>
2019-01-15 13:27:05 +0100

    init vue translator webpage

---
+++ Vue/Translator/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Translator</h1>
+    <h5>Powered by Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+  </div>
+</template>
+
+<script>
+import TranlateForm from "./components/TranslateForm";
+export default {
+  name: "App",
+  components: {
+    TranlateForm
+  },
+  methods: {
+    translateText: function(text) {}
+  }
+};
+</script>
+
+<style>
+#app {
+}
+</style>
